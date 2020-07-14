2071b685a604b684b1d68572e87544b02461c11b
Samuel <satch@Samuels-MBP.home>
2020-07-14 12:17:26 -0400

    preliminary starter pack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("starting up!");
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+
+function update() {
+  $('#main').html(`you've been on this page for ${num} seconds...`);
+  num += 1;
+}
+
+setInterval(update, 1000);
